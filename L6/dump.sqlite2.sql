54f9bbc12cfca553a90013d3a42bede10d9542fc
l1r1k4 <l1r1k4@mail.ru>
2023-12-19 09:54:36 +1000

    L6

---
+++ L6/dump.sqlite2.sql
@@ -0,0 +1,15 @@
+-- TABLE
+CREATE TABLE worker (
+    worker_id INTEGER PRIMARY KEY,
+    shop_id INTEGER REFERENCES product (id),
+    name VARCHAR(255),
+    salary INTEGER NOT NULL,
+    position VARCHAR(255)
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
